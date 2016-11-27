48fb3adcf32cb90a897857b2ddab086fede02f01
Tigran <hambardzumyan.tigr@gmail.com>
2016-11-27 22:21:00 +0400

    commit

--- http/main.cpp
+++ http/main.cpp
@@ -7,8 +7,11 @@ int main(){
 
 const char  http[] ="http://example.com/aa/bb?n=4&t=8" ;
 const char protocol_separator = '/';
+const char separators[] = [':' , '/' , '?' ,'&'];
 
 cout<<strrchr(http ,protocol_separator );
+cout<<strlen(http);
+
 
 //cout<<http;
 
