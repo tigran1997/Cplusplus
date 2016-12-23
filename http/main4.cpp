7b703d8c8db2db555aeb970769807ac7c7f2b054
Tigran <hambardzumyan.tigr@gmail.com>
2016-12-22 23:09:27 -0800

    commit

--- http/main4.cpp
+++ http/main4.cpp
@@ -1,21 +1,57 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 
 int main(){
 
 char * t;
-//const char ex2[] =  "http://example.com";
-const char  http1[] ="http:example.com/aa/bb?n=4&t=8" ;
-string http = "http://example.com/aa/bb?n=4&t=8";
+const char ex2[] =  "http://example.com";
+const char  http1[] ="http:example.com/index.php/?n=4&t=8" ;
+
+string http = "https://example.com/aa/hh/cc/bb?n=4&t=8&kk=8";
 string protocol_separator = "://";
 string variable_sseparator = "?";
 string variable_separator = "&";
+
+
 size_t prot =  http.find(protocol_separator );
-//cout<<http.substr(0 , prot);
-cout<<http.substr(prot ,3);
-cout<<http.compare(prot  , 3 , "://"); // qcel try-i mej
-//const char protocol_separator[] = "://";
+string protocol = http.substr(0 , prot); // http
+string http_sep = http.substr(prot ,3); // ://
+//cout<<http.compare(prot  , 3 , http_sep); // qcel try-i mej  if
+string uri = http.substr(prot+3); // example.com/aa/bb?n=4&t=8
+//cout<<uri;
+
+stringstream  div(uri);
+string segment;
+//cout<<string::npos;
+while(getline(div, segment, '/'))
+{
+    cout<<segment<<endl;
+}
+
+if(uri.find("?") != string::npos ){
+    cout<<uri.find("?");
+}
+string vars = uri.substr(uri.find("?")+1 , uri.length() -  uri.find("?") );
+cout<<vars;
+
+stringstream vals(vars);
+string value;
+while(getline(vals, value, '&'))
+{
+    cout<<value<<endl;
+}
+
+
+
+
+
+
+
+
+
+//const char protocol_separator[] = ":0//";
 
 }
