7b31758c294418b345c1c0673e72caa3ce570683
Tigran <hambardzumyan.tigr@gmail.com>
2016-12-25 03:33:51 -0800

    commiy

--- http/main0.cpp
+++ http/main0.cpp
@@ -5,46 +5,47 @@
 #include <vector>
 #include <algorithm>
 #include <exception>
+#include <fstream>
 using namespace std;
 
 
 class http{
 
 private:
-      string protocol_separator = "://";
-    string variable_sseparator = "?";
-    string variable_separator = "&";
-    string equal_separator = "=";
+    std::string protocol_separator = "://";
+    std::string variable_sseparator = "?";
+    std::string variable_separator = "&";
+    std::string equal_separator = "=";
 
-    string url;
-    string protocol;
-    string uri;
-    string vars;
-    map<string , string> variables;
-    string variables_string;
+    std::string url;
+    std::string protocol;
+    std::string uri;
+    std::string vars;
+    std::map<std::string , std::string> variables;
+    std::string variables_string;
 public :
-    http(string get_url){
+    http(std::string get_url){
         url = get_url;
         configureProtocol();
+        configureVaraible();
+    }
+    http (){
+
     }
 
-    string  showProtocol(){
+    std::string  showProtocol(){
         return protocol;
     }
     void configureProtocol(){
         size_t prot =  url.find(protocol_separator );
         protocol = url.substr(0 , prot); // http
-        string http_sep = url.substr(prot ,3); // ://
+        std::string http_sep = url.substr(prot ,3); // ://
+        url.compare(prot , 3 , http_sep);
 
-        try {
-            url.compare(prot , 3 , http_sep);
-        } catch (...) {
-            cout<<"Something went wrong";
-        }
         uri = url.substr(prot+3);
     }
     bool checkVarState(){
-        if(uri.find("?") != string::npos ){
+        if(uri.find("?") != std::string::npos ){
             //cout<<uri.find("?");
             return true;
         }
@@ -53,44 +54,73 @@ public :
         if(checkVarState()){
                 vars = uri.substr(uri.find("?")+1 , uri.length() -  uri.find("?") );
 
-                stringstream vals(vars);
-                string getval;
-                vector<string> variables_vector;
+                std::stringstream vals(vars);
+                std::string getval;
+                std::vector<std::string> variables_vector;
 
                 while(getline(vals, getval, '&'))
                 {
                     variables_vector.push_back(getval);
-                    stringstream allval(getval);
-                    string key;
-                    string value;
+                    std::stringstream allval(getval);
+                    std::string key;
+                    std::string value;
                     getline(allval, key, '=');
                     getline(allval, value, '=');
-                    variables.insert ( std::pair<string,string>(key, value) );
+                    variables.insert ( std::pair<std::string,std::string>(key, value) );
                 }
 
         } else{
-            cout<<"No vars";
+            //return false;
         }
     }
 
-    string showvariables(){
-        configureVaraible();
-        std::map<string,string>::iterator it = variables.begin();
+    std::string showvariables(){
+        std::map<std::string,std::string>::iterator it = variables.begin();
         for (it=variables.begin(); it!=variables.end(); ++it)
             variables_string.append(it->first + " => " + it->second +'\n' );
-            //std::cout << it->first << " => " << it->second << '\n';
         return variables_string;
     }
+
+    std::string showDomein(){
+        if(uri.find('/')  != std::string::npos){
+            size_t dm = uri.find_first_of('/');
+            return uri.substr(0,dm);
+        } else{
+            return uri;
+        }
+
+
+
+
+    }
+    friend ostream& operator<<(ostream&, http&);
+
+	friend istream& operator >> (istream&, http&);
 };
+
+std::istream& operator >> (istream& in, http& s)
+{
+
+    return in;
+}
+
+std::ostream& operator<<(ostream& out, http& s)
+{
+    out << s.url<<endl;
+}
+
+
 
 
 int main(){
-
-htpp h;
-cin >> h;
+//htpp h;
+//cin >> h;
 http a("wss://example.com/aa/hh/cc/bb?a&rr=66");
-cout<<a.showProtocol();
+cin>>a;
+cout<<a;
+std::cout<<a.showProtocol();
 //a.configureVaraible();
-cout << a;
-cout<<a.showvariables();
+//cout << a;
+std::cout<<a.showvariables();
+std::cout<<a.showDomein();
 }
